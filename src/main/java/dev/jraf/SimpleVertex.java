8421d0a6f4c4596b2ae7f08818dbde6af028e0a0
MORON USON guillermo <guillermo.moron@laposte.net>
2024-01-10 10:53:00 +0100

    Updated documentation and added javadoc plugin

--- src/main/java/dev/jraf/SimpleVertex.java
+++ src/main/java/dev/jraf/SimpleVertex.java
@@ -1,14 +1,29 @@
 package dev.jraf;
 
+/**
+ * A simple implementation of {@code Vertex}.
+ *
+ * @author Guillermo Morón Usón
+ */
 public class SimpleVertex implements Vertex {
 
+    // Messages to show when an exception is thrown
     private static final String ILLEGAL_LABEL_LENGTH_MSG = "label must have a "
         + "length superior or equal to 1 and strictly inferior to 256";
     private static final String NULL_LABEL_MSG = "label must be non-null";
+
+    // Constants
     private static final int MAX_LENGTH_LABEL = 255;
 
+    // The label
     private final String label;
 
+    /**
+     * Class constructor specifying the label. The label must be a non-null
+     * string of length superior or equal to 1 and strictly inferior to 256.
+     *
+     * @param label the label for the new instance
+     */
     public SimpleVertex(String label) {
         if (label == null) {
             throw new NullPointerException(NULL_LABEL_MSG);
@@ -19,6 +34,9 @@ public class SimpleVertex implements Vertex {
         this.label = label;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override public String label() {
         return this.label;
     }
--- src/main/java/dev/jraf/Vertex.java
+++ src/main/java/dev/jraf/Vertex.java
@@ -1,5 +1,16 @@
 package dev.jraf;
 
+/**
+ * A vertex of a graph, labeled with a string.
+ *
+ * @author Guillermo Morón Usón
+ */
 public interface Vertex {
+
+    /**
+     * Returns the label of this {@code Vertex}.
+     *
+     * @return a string, the label of this {@code Vertex}
+     */
     String label();
 }
